b9c809954b8978390fa9c3ccae5754073c221720
Preasys <52882498+Preasys@users.noreply.github.com>
2023-02-12 23:26:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-steamlit.text('Hard-Boiled Free-Ranged Eggs')
+streamlit.text('Hard-Boiled Free-Ranged Eggs')
 
 
 
