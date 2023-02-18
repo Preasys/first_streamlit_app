f086e64c926ffe5daf8b9df069f006bbce789636
Preasys <52882498+Preasys@users.noreply.github.com>
2023-02-18 21:00:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,11 @@ my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
+streamlit.dataframe(my_data_row)
+
+
+streamlit.dataframe(my_data_rows)
+
 
 
 
