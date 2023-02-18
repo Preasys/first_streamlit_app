9cb94a70d642876410f7659222de300a0b15cc84
Preasys <52882498+Preasys@users.noreply.github.com>
2023-02-18 21:04:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-my_cur.execute("select * from pc_rivery_db.public.fruityvice")
+my_cur.execute("select * from pc_rivery_db.public.FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
